a06347db27871686f37f801ca4f2c60f0cc01d4a
LeonidKhoroshev <114744186+LeonidKhoroshev@users.noreply.github.com>
2023-12-01 10:35:12 +0300

    Update variables02.tf

--- 02/src/variables02.tf
+++ 02/src/variables02.tf
@@ -45,23 +45,37 @@ variable "vm_web_family" {
   type        = string
   default     = "ubuntu-2004-lts"
   }
-
 variable "vm_web_name" {
   type        = string
   default     = "netology-develop-platform-web"
   }
-
 variable "vm_web_platform_id" {
   type        = string
   default     = "standard-v2"
   }
-
 variable "vm_web_recources_core" {
   type        = number
   default     = "2"
   }
-
 variable "vm_web_recources_memory" {
   type        = number
   default     = "2"
+  }
+variable "vm_web_recources_core_fraction" {
+  type        = number
+  default     = "20"
+  description = "guaranteed vCPU, for yandex cloud - 20, 50 or 100 "
+  }
+
+variable "vm_web_scheduling_policy_preemptible {
+  type        = bool
+  default     = "true"
+
+variable "vm_web_boot_disk_image_id" {
+  type        = string
+  default     = "data.yandex_compute_image.ubuntu.image_id"
+  }
+variable "vm_web_network_interface_network_id" {
+  type        = string
+  default     = "yandex_vpc_subnet.develop.id"
   }
