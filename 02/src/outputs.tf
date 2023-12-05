439e29d981dda30c7f7b3805d43e6bdcb5260d74
LeonidKhoroshev <114744186+LeonidKhoroshev@users.noreply.github.com>
2023-12-05 15:42:48 +0300

    Update outputs.tf

--- 02/src/outputs.tf
+++ 02/src/outputs.tf
@@ -1,10 +1,7 @@
-output "vm_external_ip_address_1" {
-  value = yandex_compute_instance.platform.network_interface.0.nat_ip_address
-  description = "netology-develop-platform-web external ip"
+output "ip_address_vm1" {
+  value = tomap({(var.vm_web_name) = (yandex_compute_instance.platform.network_interface.0.nat_ip_address)})
 }
 
-
-output "vm_external_ip_address_2" {
-  value = yandex_compute_instance.platform2.network_interface.0.nat_ip_address
-  description = "netology-develop-platform-web external ip"
+output "ip_address_vm2" {
+  value = tomap({(var.vm_db_name) = (yandex_compute_instance.platform2.network_interface.0.nat_ip_address)})
 }
