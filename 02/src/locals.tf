9315c6eb19f3d684f7f034d2f44b5b3e4f950e13
LeonidKhoroshev <114744186+LeonidKhoroshev@users.noreply.github.com>
2023-12-06 22:53:49 +0300

    Update locals.tf

--- 02/src/locals.tf
+++ 02/src/locals.tf
@@ -1,7 +1,4 @@
 locals {
-  instance             = "netology-develop-platform"
-  name_web             = "web"
-  name_db              = "db"
-  vm_web_instance_name = "${local.instance}-${local.name_web}"
-  vm_db_instance_name  = "${local.instance}-${local.name_db}"
+  vm_web_instance_name = "${var.instance}-${var.name_web}"
+  vm_db_instance_name  = "${var.instance}-${var.name_db}"
 }
