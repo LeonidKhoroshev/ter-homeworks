24d4b5c80ed5915bc78919fe82ddb193f1079974
Leonid Khoroshev <khoroshevlv@gmail.com>
2023-12-16 23:49:40 +0300

    terraform-03

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,21 @@
+
 output "web" {
-   value = {
-     for k,v in yandex_compute_instance.web:
-     k => tomap({fqdn=v.fqdn,id=v.id,name=v.name})
+ value = {
+     for k, v in yandex_compute_instance.web:
+     k => ({fqdn=v.fqdn,id=v.id,name=v.name})
      }
-   }
+}
 
 output "vm" {
-   value = {
+ value = {
      for k,v in yandex_compute_instance.vm:
-     k => tomap({fqdn=v.fqdn,id=v.id,name=v.name})
+     k => ({fqdn=v.fqdn,id=v.id,name=v.name})
      }
-   }
+}
+
+output "storage" {
+ value = {
+     for k,v in [yandex_compute_instance.storage]:
+     k => ({fqdn=v.fqdn,id=v.id,name=v.name})
+     }
+}
