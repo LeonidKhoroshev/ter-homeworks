8d8da8d16aca8b606d9262053980209c4a780911
Leonid Khoroshev <khoroshevlv@gmail.com>
2023-12-31 16:17:26 +0300

    exersice 5

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,5 @@
-
-output "web" {
-  value = [for server in yandex_compute_instance.web:{fqdn = server.fqdn, id = server.id, name = server.name }]
+output "servers" {
+  value = [[for server in yandex_compute_instance.web:{fqdn = server.fqdn, id = server.id, name = server.name }], 
+           [for server in yandex_compute_instance.vm:{fqdn = server.fqdn, id = server.id, name = server.name }],        
+           [for server in [yandex_compute_instance.storage]:{fqdn = server.fqdn, id = server.id, name = server.name }]]
 }
-
-output "vm" {
-  value = [for server in yandex_compute_instance.vm:{fqdn = server.fqdn, id = server.id, name = server.name }]        
-}
-
-output "storage" {
-  value = [for server in [yandex_compute_instance.storage]:{fqdn = server.fqdn, id = server.id, name = server.name}]
-}
-
- 
