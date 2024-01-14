5ef8b34f674ff1b020ac37d98b5185d75450af5f
LeonidKhoroshev <114744186+LeonidKhoroshev@users.noreply.github.com>
2024-01-14 21:05:56 +0300

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -5,7 +5,24 @@ terraform {
     }
   }
   required_version = ">=0.13"
+
+ backend "s3" {
+    endpoint                    = "storage.yandexcloud.net"
+    bucket                      = "yc-leo"
+    region                      = "ru-central1"
+    key                         = "terraform.tfstate"
+    access_key                  = "YCAJEvxktd888WIkZLA4tjF09"
+    secret_key                  = "YCNNxI7GfU2FN501HQa6ot8Z4OWcq7rATxCOjqcz"
+    dynamodb_endpoint           = "grpcs://ydb.serverless.yandexcloud.net:2135/?database=/ru-central1/b1g3ks25rm2qagep03qb/etnsarqlsjqonfk97bo2"
+    dynamodb_table              = "table137"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+ }
+
 }
+
+
+
 
 provider "yandex" {
   token     = var.token
