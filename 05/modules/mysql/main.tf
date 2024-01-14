c3d66362a78d3cd594d524803728f78317f571df
LeonidKhoroshev <114744186+LeonidKhoroshev@users.noreply.github.com>
2024-01-14 14:18:58 +0300

    Update main.tf

--- 05/modules/mysql/main.tf
+++ 05/modules/mysql/main.tf
@@ -10,10 +10,18 @@ resource "yandex_mdb_mysql_cluster" "example" {
     resource_preset_id = var.resource_preset_id
     disk_type_id       = var.disk_type
     disk_size          = var.disk_size
-  }  
-
-   host {
-    zone               = var.zone
-    subnet_id         =  var.subnet_ids
   }
+
+
+  dynamic "host" {
+    for_each          =  var.HA == true ? [2]:[1]
+    content {
+    zone              =  var.zone
+    subnet_id         =  var.subnet_ids
+    }
+  }
+
 }
+
+
+
