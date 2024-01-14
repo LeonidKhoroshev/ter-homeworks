525b7dc991d98854d7d8f91939ffb01ee3eef45b
LeonidKhoroshev <114744186+LeonidKhoroshev@users.noreply.github.com>
2024-01-14 14:25:00 +0300

    Update main.tf

--- 05/main.tf
+++ 05/main.tf
@@ -3,3 +3,22 @@ module "vpc_db" {
   env_name        = var.env_name
   subnets         = var.subnets
 }
+
+module "cluster" {
+  source          = "./modules/mysql"
+  cluster_name    = var.cluster_name
+  network_id      = var.network_id
+  subnet_ids      = var.subnet_id
+  security_group  = var.securiry_group
+  HA              = var.HA
+}
+
+module "db" {
+  source          = "./modules/database"
+  database_name   = var.db_name
+  user_name       = var.user_name
+  network_id      = var.network_id
+  subnet_ids      = var.subnet_id
+  my_cluster_id   = var.my_cluster_id
+}
+
